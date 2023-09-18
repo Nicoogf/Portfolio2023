c51b0267565993000dc889e9758ebb6428906d53
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-18 13:34:03 -0300

    Avance: Portolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,50 @@
+import Work1 from "../../assets/work-1.svg" ;
+import Work2 from "../../assets/work-2.svg" ;
+import Work3 from "../../assets/work-3.svg" ;
+import Work4 from "../../assets/work-4.svg" ;
+import Work5 from "../../assets/work-5.svg" ;
+import Work6 from "../../assets/work-6.svg" ;
+
+
+const Menu = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Project Managment ilustration",
+        category: "Desing"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "App de screens",
+        category: "Art"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "delivery app",
+        category: "Branding"
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "onbrabndi motivation",
+        category: "Desing"
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "ImacMock desing",
+        category: "Creative"
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "GameStore",
+        category: "art"
+    },
+
+] ;
+
+
+export default Menu;
