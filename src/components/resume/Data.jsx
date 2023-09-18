aab5dafe48247fc49e4626686d4c3bcc6009fc0f
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-18 11:59:30 -0300

    Avance: Creacion de Card

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,59 @@
+const data = [
+
+    {
+        id: 1,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    },
+
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    },
+
+    {
+        id: 3,
+        category: "Experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    },
+
+    {
+        id: 4,
+        category: "Experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    },
+
+    {
+        id: 5,
+        category: "Experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    },
+
+    {
+        id: 6,
+        category: "Experience",
+        icon: "icon-briefcase",
+        year: "2009 - 2013",
+        title: "Back-End Developer",
+        desc: "Lorem bla bla bla bla bla bla bla"
+    }
+
+
+]
+export default data ;
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,55 @@
 import React from 'react' ;
 import "./resume.css" ;
+import Data from "./Data" ;
+import Card from './Card';
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <section className="resume container section" id="resume">
+      <h2 className='section__title'> Experience</h2>
+
+      <div className="resume_container grid">
+
+        <div className="timeline grid">
+          { Data.map((item)=> {
+            if(item.category === "education"){
+
+                return(
+                  <Card 
+                      key={ item.id } 
+                      icon={ item.icon } 
+                      title={ item.title } 
+                      year={ item.year } 
+                      desc={ item.desc }
+                  />
+                )
+
+            }
+           })}
+        </div>
+
+        <div className="timeline grid">
+          { Data.map((item)=> {
+            if(item.category === "experience"){
+
+                return(
+                  <Card 
+                      key={ item.id } 
+                      icon={ item.icon } 
+                      title={ item.title } 
+                      year={ item.year } 
+                      desc={ item.desc }
+                  />
+                )
+            }
+           })}
+        </div>
+
+
+      
+
+      </div>
+    </section>
   ) ;
 } ;
 
